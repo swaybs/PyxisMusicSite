822bc2c0905308ca33fff56c30043f2cc65d4e65
Andrew Rothbauer <andrew.rothbauer@gmail.com>
2018-12-14 13:46:47 -0500

    Adding shadows with OkShadow

---
+++ js/shadow.js
@@ -0,0 +1,11 @@
+$(function(){
+     $('.random-text').okshadow({
+       color: '#555',
+       textShadow: true,
+       xMax: 5,
+       yMax: 0,
+       yOffset: 3,
+       fuzzMin: 1,
+       fuzzMax: 3,
+     });
+   });
