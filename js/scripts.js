23e93a4d87594d4cb854e5c51adfb42acf325657
Andrew Rothbauer <andrew.rothbauer@gmail.com>
2018-12-20 16:05:04 -0500

    adding more n' more

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
 
 
+
+
 // BURGER ICON FUNCTION
 function burger(x) {
   x.classList.toggle('change');
 }
 
+// Toggle function for burger button
 function navToggle() {
   var overlay = document.getElementById('site-nav-primary');
 
   if (overlay.style.height === "100%") {
     overlay.style.height = "0%";
+
   } else {
     overlay.style.height = "100%";
+    overlay.style.right = "50%";
   }
 }
