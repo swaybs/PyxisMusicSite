564bc402f4ab5cb78b947a51f33729f4736f85e5
Andrew Rothbauer <andrew.rothbauer@gmail.com>
2019-01-04 14:24:03 -0500

    Flashing Name install

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,43 @@ function navToggle() {
     overlay.style.right = "50%";
   }
 }
+
+// Flashing function
+
+
+
+fadeloop('.name-part.p', 1500, 500, true)
+fadeloop('.name-part.y', 900, 300, true)
+fadeloop('.name-part.x', 1000, 3200, true)
+fadeloop('.name-part.i', 800, 2200, true)
+fadeloop('.name-part.s', 200, 1400, true)
+
+function fadeloop(el,timeout,timein,loop){
+    var $el = $(el),intId,fn = function(){
+         $el.fadeOut(timeout).fadeIn(timein);
+    };
+    fn();
+    if(loop){
+        intId = setInterval(fn,timeout+timein+100);
+        return intId;
+    }
+    return false;
+}
+
+// Loop from 0% width to 100% width
+
+// $('document').ready(function(){
+//   $('.name-home').css('width', '0%');
+// })
+
+// jQuery FadeIn one after the other
+
+// $('document').ready(function(){
+//   $('.name-part').css('display', 'none');
+//   $('.name-part.p').delay(500).addClass('flipInX').fadeIn(500);
+//   $('.name-part.y').delay(500).addClass('flipInX').fadeIn(1000);
+//   $('.name-part.x').delay(500).addClass('flipInX').fadeIn(1500);
+//   $('.name-part.i').delay(500).addClass('flipInX').fadeIn(2000);
+//   $('.name-part.s').delay(500).addClass('flipInX').fadeIn(2500);
+//
+// })
